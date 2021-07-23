c9e49610407751d5bf46fe31169b7f9870f32675
Rasha Haneche <rashahaneche@MacBook-Pro-de-Rasha.local>
2021-07-23 19:36:15 +0200

    Tenemos el endpoint order que devuelve los pedidos por id

---
+++ controllers/orderController.js
@@ -0,0 +1,34 @@
+// Importamos modelo
+const Order = require('../models/OrderModel.js');
+
+// Funcion para gestionar get
+const getOrder = async (req, res) => {
+	const Orders = await Order.find();
+	res.send (Orders);
+}
+
+// Funcion para gestionar post
+const postOrder = (req, res) => {
+
+	const {seller,buyer,dish,date,completed} = req.body;
+	const newOrder= new Order();
+
+	newOrder.seller = seller;
+	newOrder.buyer = buyer;
+	newOrder.dish = dish;
+    newOrder.date= date;
+    newOrder.completed=completed;
+
+	newOrder.save ((err, savedInfo) => {
+		if (err){
+			console.log('Error añadiendo ejemplo', err);
+		}
+		res.json(savedInfo);
+	});
+}
+
+// Exportamos como objeto
+module.exports = {
+	getOrder,
+	postOrder
+}
---
+++ routes/orderRouter.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+// Importamos controlador
+const orderController = require('../controllers/orderController.js');
+
+const router = express.Router();
+
+
+router.get('/:id', orderController.getOrder);
+router.post('/', orderController.postOrder);
+
+module.exports = router;
